763e3151aa9f95e4f889708e4c07998d0177c935
Colin Snyder <86389945+colinstartneon@users.noreply.github.com>
2022-03-07 21:19:57 -0800

    no to polygons

--- src/Components2/EventsComponents/Tooling.js
+++ src/Components2/EventsComponents/Tooling.js
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import Iframe from "react-iframe";
-<script type="module" src="https://unpkg.com/x-frame-bypass"></script>;
+import blackx from "../../Images2/blackx.svg";
+import { Stage, Layer, Line } from "react-konva";
+import _flatten from "lodash/flatten";
 
 export default function Tooling() {
+  const [goBack, setGoBack] = useState(false);
+
   useEffect(() => {
     document.body.style.overflow = "hidden";
   }, []);
@@ -11,40 +15,109 @@ export default function Tooling() {
   return (
     <Wrapper>
       <BlueBlur>
-        <WhiteRectangle />
         <PositionDiv>
-          <WhiteRectangle>hi</WhiteRectangle>
-          <Iframe
-            url="http://localhost:3000/?url=https://www.tesla.com/"
-            width="2000px"
-            height="1540px"
-            id="myId"
-            className="myClassname"
-            display="initial"
-            position="relative"
-          ></Iframe>
+          <Rectangle3 />
+          <InsideRectangle>
+            <BlackX src={blackx} />
+          </InsideRectangle>
+          <NoClick>
+            <Iframe
+              url="https://www.banabo.io"
+              width="2000px"
+              height="1540px"
+              id="myId"
+              className="myClassname"
+              display="initial"
+              position="relative"
+            >
+              {" "}
+            </Iframe>
+          </NoClick>
         </PositionDiv>
       </BlueBlur>
     </Wrapper>
   );
 }
 
+const TopLine = styled.div`
+  z-index: 30;
+`;
+
 const Wrapper = styled.div`
   overflow-y: hidden;
 `;
 
-const WhiteRectangle = styled.div`
+const NoClick = styled.div`
+  pointer-events: none;
+`;
+
+const BlackX = styled.img`
+  padding-top: 15px;
+  padding-left: 14px;
+  cursor: pointer;
+  z-index: 30;
+`;
+
+const PositionDiv2 = styled.div`
+  z-index: 200;
   position: absolute;
-  width: 100vw;
-  height: 34px;
+  right: 0px;
+  bottom: 0px;
+`;
+
+const WhiteText = styled.div`
+  font-size: 50px;
+  line-height: 61px;
+  font-family: "ProximaNovaBold";
+  color: #ffffff;
+`;
+
+const Rectangle3 = styled.div`
+  position: absolute;
+  left: 1680px;
+  top: 120px;
+  width: 200px;
+  height: 44px;
+  background: #332c58;
+  border-radius: 10px;
+  z-index: 20;
+`;
+
+const Rectangle4 = styled.div`
+  position: absolute;
+  left: 1280px;
+  top: 40px;
+  width: 200px;
+  height: 44px;
+  background: #332c58;
+  border-radius: 10px;
+  z-index: 20;
+`;
+
+const InsideRectangle = styled.div`
+  position: absolute;
+  left: 1900px;
+  top: 120px;
+  width: 42px;
+  height: 42px;
+  background: #ffffff;
+  border-radius: 10px;
+  z-index: 20;
+`;
+
+const Rectangle1 = styled.div`
+  position: absolute;
+  width: 2000px;
+  height: 60px;
   left: 80px;
-  top: 45px;
+  top: 44px;
   background: #ffffff;
   display: flex;
-  justify-content: center;
+  justify-content: right;
   align-items: center;
   cursor: pointer;
   z-index: 50;
+  padding-right: 10px;
 `;
 
 const PositionDiv = styled.div`
